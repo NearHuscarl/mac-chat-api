bb0574034d31654b5c62136f1d7d4c34b87b53de
NearHuscarl <near.huscarl@gmail.com>
2018-09-14 01:16:37 +0700

    setup local development port and mongouri

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,20 @@
 import mongodb from 'mongodb';
 
-export default {
-  // "port": 3005,
-  // "mongoUrl": "mongodb://localhost:27017/chat-api",
-  "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
-  "bodyLimit": "100kb"
+console.log(process.env.NODE_ENV);
+
+let port = undefined;
+let mongoUrl = undefined;
+
+if (process.env.NODE_ENV === 'development') {
+  port = 3005;
+  mongoUrl = 'mongodb://localhost:27017/chat-api';
+} else {
+  port = process.env.PORT;
+  mongoUrl = process.env.MONGODB_URI;
 }
+
+export default {
+  port: port,
+  mongoUrl: mongoUrl,
+  bodyLimit: '100kb',
+};
